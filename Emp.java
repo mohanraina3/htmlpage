13d2cfa31a5322116a370a7f6ecc9c3f4c2fda3b
mohanraina3 <152164608+mohanraina3@users.noreply.github.com>
2023-11-29 15:59:43 +0530

    Add files via upload

---
+++ Emp.java
@@ -0,0 +1,33 @@
+package Bat;
+
+public class Emp {
+private String id;	
+private String name;
+private String jersey;
+private String country;
+
+public String getId() {
+	return id;
+}
+public String getName() {
+	return name;
+}
+public String getJersey() {
+	return jersey;
+}
+public String getCountry() {
+	return country;
+}
+public void setId(String id) {
+	this.id=id;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public void setJersey(String jersey) {
+	this.jersey = jersey;
+}
+public void setCountry(String country) {
+	this.country = country;
+}
+}
